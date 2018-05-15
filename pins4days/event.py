a91559adb4c97194acb33257330357f3e17c06a7
susana <susana@users.noreply.github.com>
2018-05-15 12:36:12 -0400

    Added docstrings to event module.

--- pins4days/event.py
+++ pins4days/event.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+"""Factory classes for creating pinsdays.models.pin.Pin and
+pinsdays.models.pin.Attachment.
+"""
 
 import logging
 from models.pin import Pin
@@ -6,8 +9,23 @@ from models.attachment import Attachment
 
 
 class PinnedMessage(object):
+
+    """Creates a Pin with Attachments based on the pin_added event sent by
+    the Slack events API.
+    """
+
     @staticmethod
     def factory(event):
+        """Creates a Pin with Attachments based on the pin_added event sent by
+        the Slack events API.
+
+        Args:
+            event (dict): A dict representation of the JSON sent by the Slack
+            events API upon the creation of a pin_added event.
+
+        Returns:
+            pins4days.models.pin.Pin: A Pin model.
+        """
         message = event['event']['item']['message']
         pinned_info = event['event']['pinned_info']
         attachments = message['attachments'] if 'attachments' in message else []
@@ -26,8 +44,26 @@ class PinnedMessage(object):
 
 
 class MessageAttachment(object):
+
+    """Creates Attachments based on the pin_added event sent by the Slack
+    events API.
+    """
+
     @staticmethod
     def factory(attachment):
+        """Creates Attachments based on the pin_added event sent by the Slack
+        events API.
+
+        Args:
+            attachment (dict): A portion of the JSON sent by the Slack events
+            API upon the creation of a pin_added event. This dict should
+            contain only the data for a single attachment. It can be
+            accessed from the event JSON/dict like so:
+            e.g. event['event']['item']['message']['attachments'][0]
+
+        Returns:
+            pins4days.models.attachment.Attachment: An Attachmnent model.
+        """
         return Attachment(
             from_url=attachment['from_url'] if 'from_url' in attachment else None,
             image_url=attachment['image_url'] if 'image_url' in attachment else None,
